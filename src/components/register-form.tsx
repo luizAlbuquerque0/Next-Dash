b42f92448b95055cfffd04f84758509d38d55f05
Luiz <luiz.albuquerque99@gmail.com>
2025-03-26 21:36:12 -0300

    feature: implementing register action

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -10,55 +10,67 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function RegisterForm({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
-  return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Register</CardTitle>
-          <CardDescription>
-            Create you account to access the plataform
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" type="text" placeholder="your name" required />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="email">Password</Label>
-                <Input id="email" type="password" placeholder="****" required />
-              </div>
+interface IRegisterFormProps {
+  registerAction: (formData: FormData) => Promise<void>;
+}
 
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
-                  Create account
-                </Button>
-              </div>
+export function RegisterForm({ registerAction }: IRegisterFormProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Register</CardTitle>
+        <CardDescription>
+          Create you account to access the plataform
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form action={registerAction}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="your name"
+                required
+              />
             </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?
-              <a href="/login" className="underline underline-offset-4">
-                Sign in
-              </a>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
             </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="****"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <Button type="submit" className="w-full">
+                Create account
+              </Button>
+            </div>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?
+            <a href="/login" className="underline underline-offset-4">
+              Sign in
+            </a>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
